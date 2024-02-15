cb6dc65eed287683724c4c4f888ce485e6a6b551
zolo1917 <adwaithvenkatesh@Adwaiths-MacBook-Pro.local>
2024-02-16 00:26:41 +0530

    udpate and the implementation

--- AccountService/src/main/java/com/diary/AccountService/Services/DebitTransactionService.java
+++ AccountService/src/main/java/com/diary/AccountService/Services/DebitTransactionService.java
@@ -2,12 +2,13 @@ package com.diary.AccountService.Services;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class DebitTransactionService {
     public List getTransactions () {
         // create debit transactions
+        return new ArrayList();
     }
-    public
 }
