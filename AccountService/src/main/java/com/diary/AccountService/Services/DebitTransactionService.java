b3afd1e1de9b5366cffa148cae505cf07a08537c
zolo1917 <adwaithvenkatesh@Adwaiths-MacBook-Pro.local>
2024-02-16 08:28:18 +0530

    update the implementation for credit and debit

--- AccountService/src/main/java/com/diary/AccountService/Services/DebitTransactionService.java
+++ AccountService/src/main/java/com/diary/AccountService/Services/DebitTransactionService.java
@@ -1,5 +1,6 @@
 package com.diary.AccountService.Services;
 
+import com.diary.AccountService.Entities.DebitTransactions;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,6 +10,23 @@ import java.util.List;
 public class DebitTransactionService {
     public List getTransactions () {
         // create debit transactions
-        return new ArrayList();
+        return null;
+    }
+
+    public DebitTransactions getDebitTransactionById(String id) {
+        return  new DebitTransactions();
+    }
+
+    public List<DebitTransactions> getTransactionsForAccount(String accountNumber){
+        return null;
+    }
+    public String createDebit () {
+        // TODO: update this impl
+        return null;
+    }
+
+    public String updateDebitDetails () {
+        //TODO: update this impleemntation
+        return null;
     }
 }
