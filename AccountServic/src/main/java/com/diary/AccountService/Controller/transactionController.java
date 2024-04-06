a74af935e419d93ac0b6e3ea33b9f20a1528afe9
zolo1917 <adwaithvenkatesh@Adwaiths-MacBook-Pro.local>
2024-04-07 01:55:58 +0530

    update detials

--- AccountServic/src/main/java/com/diary/AccountService/Controller/transactionController.java
+++ AccountServic/src/main/java/com/diary/AccountService/Controller/transactionController.java
@@ -1,6 +1,5 @@
 package com.diary.AccountService.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- dashboard/src/Components/Accounts/Accounts.jsx
+++ dashboard/src/Components/Accounts/Accounts.jsx
@@ -1,9 +1,76 @@
+import {
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import CommentIcon from "@mui/icons-material/Comment";
+
 function Accounts() {
-    return (
-        <div>
-            This is the accounts page
-        </div>
-    )
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={4} sx={{ height: "30vh" }}>
+        <Card sx={{ height: "100%" }}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Accounts
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={4} sx={{ height: "30vh" }}>
+        <Card sx={{ height: "100%" }}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Cards
+            </Typography>
+            <List>
+              <ListItem>
+                <ListItemText
+                  primary={"XXXX23241"}
+                  secondary={"new Text2"}
+                ></ListItemText>
+                <Typography>5323.23</Typography>
+              </ListItem>
+              <Divider></Divider>
+            </List>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={4}>
+        <Card sx={{ height: "100%" }}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Loans
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={6} sx={{ height: "60vh" }}>
+        <Card sx={{ height: "100%" }}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Expense Analytics
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={6} sx={{ height: "50vh" }}>
+        <Card sx={{ height: "100%" }}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Budget Analytics
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Accounts;
