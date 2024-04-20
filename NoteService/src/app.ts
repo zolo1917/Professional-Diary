ffe7aaf77b86bdd8c86e48dd6f132c5830321ab4
zolo1917 <adwaithvenkatesh@Adwaiths-MacBook-Pro.local>
2024-04-20 13:44:44 +0530

    update login

--- NoteService/src/app.ts
+++ NoteService/src/app.ts
@@ -1,14 +1,36 @@
-import express, { Express } from "express";
+import express, { Express, NextFunction, Response } from "express";
 import * as dotenv from "dotenv";
 import { connectMongo } from "./config/dbconfig";
 import { notesRouter } from "./api/notes";
 import cors from "cors";
+import { secretKey } from "./config/config";
+import * as jwt from "jsonwebtoken";
 const app: Express = express();
 app.use(cors());
 app.use(express.json());
-app.use(notesRouter);
+app.use((req: any, resp: Response, next: NextFunction) => {
+  const token = req.header("Authorization")?.split(" ")[1];
+  if (token) {
+    let key = secretKey() || "";
+    jwt.verify(token, key, (err: any, user: any) => {
+      if (!err) {
+        console.log("token Validated");
+        req.user = user;
+        next();
+      } else {
+        console.log(`error Occured: ${err}`);
+        resp.status(401).json({ message: "Authentication required." });
+        resp.send();
+      }
+    });
+  } else {
+    resp.status(401).json({ message: "Authentication required." });
+    resp.send();
+  }
+});
 dotenv.config();
 connectMongo();
+app.use(notesRouter);
 const port = process.env.PORT || 3000; // 4301
 app.listen(port, () => {
   console.log(`Application is running on Port: ${port}`);
