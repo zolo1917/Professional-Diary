9156b74acabc6cffcb43ea7d2f028e129c3046ca
zolo1917 <adwaith_venkatesh@msn.com>
2024-05-26 13:55:40 +0530

    update test docker push

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     } else {
       navigate("/homepage");
     }
-  }, userDetails);
+  }, [userDetails]);
   const handleDrawerToggle = () => {
     setDrawerState((drawerState) => {
       return !drawerState;
